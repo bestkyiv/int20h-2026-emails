013f39edb6c4dae19a8ab211bd2a6c3829116f43
Ivan Dutov <ivan.dutov.work@gmail.com>
2026-01-27 17:47:11 +0200

    feat: more or less outlook-friendly version

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
         pixeloid: ["Pixeloid Sans", "Nunito Sans", "sans-serif"],
         nunito: ["Nunito Sans", "sans-serif"],
       },
+      fontSize: {
+        md: "14px",
+        lg: "20px",
+      },
+      dropShadow: {
+        soft: "0 4px 40px rgba(239, 239, 239, 0.25)",
+        softPink: "0 4px 40px rgba(246, 65, 123, 0.25)",
+      },
     },
   },
 };
